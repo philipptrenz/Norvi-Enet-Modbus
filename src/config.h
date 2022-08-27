f4640a6b6cfb93b11df0fa3b092f844a5a44abb6
Philipp Trenz <mail@philipptrenz.de>
2022-08-27 10:23:16 +0200

    improve code formatting

--- src/config.h
+++ src/config.h
@@ -3,13 +3,10 @@
  */
 uint8_t eth_MAC[] = { 0x02, 0xF0, 0x0D, 0xBE, 0xEF, 0x01 };
 
-
 /*
- * Define the static network settings for this gateway's ETHERNET connection
- * on your LAN.  These values must match YOUR SPECIFIC LAN.  The "eth_IP"
- * is the IP address for this gateway's ETHERNET port.
+ * Network configuration.
  */
-IPAddress eth_IP(192, 168, 1, 252);		// *** CHANGE THIS to something relevant for YOUR LAN. ***
-IPAddress eth_MASK(255, 255, 255, 0);	// Subnet mask.
-IPAddress eth_DNS(192, 168, 1, 1);		// *** CHANGE THIS to match YOUR DNS server.           ***
-IPAddress eth_GW(192, 168, 1, 1);		// *** CHANGE THIS to match YOUR Gateway (router).     ***
+IPAddress eth_IP(192, 168, 1, 252);     // Static device IP address.
+IPAddress eth_MASK(255, 255, 255, 0);   // Subnet mask.
+IPAddress eth_DNS(192, 168, 1, 1);      // DNS server IP address.
+IPAddress eth_GW(192, 168, 1, 1);       // IP address of network gateway.
